64baf1b65504814edc61d594f94f8e2d41ac73b9
jisooooooooooo <dlawltn0708@naver.com>
2024-01-19 12:21:16 +0900

    1316 그룹 단어 체커

---
+++ Silver/1316 그룹 단어 체커.py
@@ -0,0 +1,14 @@
+n = int(input())
+c = n
+
+for i in range(n):
+    w = input()
+    for j in range(len(w)-1):
+        if w[j] == w[j+1]:
+            continue
+        elif w[j] in w[j+1:]:
+            c -= 1
+            break
+
+
+print(c)
