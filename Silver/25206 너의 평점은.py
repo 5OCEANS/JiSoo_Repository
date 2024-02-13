3612a1bb469e8508a612a8b15b564dd1806896bb
jisooooooooooo <dlawltn0708@naver.com>
2024-02-13 11:15:27 +0900

    25206 너의 평점은

---
+++ Silver/25206 너의 평점은.py
@@ -0,0 +1,13 @@
+dd = {"A+":4.5, "A0":4.0, "B+":3.5, "B0":3.0, "C+":2.5, "C0":2.0, "D+":1.5, "D0":1.0, "F":0.0}
+
+
+t = 0
+r = 0
+for i in range(20):
+    a, b, c = input().split()
+    b = float(b)
+    if c != 'P':
+        t += b
+        r += b * dd[c]
+
+print('%.6f' % (r/t))
