b21db04a8961f03f7819b233bd908447b8070f9d
jisooooooooooo <dlawltn0708@naver.com>
2024-02-28 13:05:03 +0900

    옷가게 할인 받기

--- programmers/Lv0/옷가게 할인 받기.js
+++ programmers/Lv0/옷가게 할인 받기.js
@@ -1,15 +1,11 @@
 function solution(price) {
   var d;
   var answer;
-  if(price >= 500000){
-    d = 0.2;
-  }
-  else if(price >= 300000){
-    d = 0.1;
-  }
-  else if(price >=100000){
-    d = 0.05;
-  }
-  answer = Math.floor(price - price*d);
-  return answer;
+  if (price >= 500000) {
+    return parseInt(price - price * 0.2);
+  } else if (price >= 300000) {
+    return parseInt(price - price * 0.1);
+  } else if (price >= 100000) {
+    return parseInt(price - price * 0.05);
+  } else return parseInt(price);
 }
