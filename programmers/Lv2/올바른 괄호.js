6142844868f7ae231ac0f24a1b308da0daa215ef
jisooooooooooo <dlawltn0708@naver.com>
2024-04-13 00:05:12 +0900

    올바른 괄호

---
+++ programmers/Lv2/올바른 괄호.js
@@ -0,0 +1,10 @@
+const solution = (s) => {
+  let result = [];
+  for(let i = 0; i < s.length; i ++){
+      if(s[i] === "(") result.push("(")
+      else 
+          if(result.length === 0) return false;
+          else result.pop();
+  }
+  return result.length === 0;
+}
