115f6563f9d1e560808621cbe4fe49323571f1b6
jisooooooooooo <dlawltn0708@naver.com>
2024-04-25 12:05:20 +0900

    콜라 문제

---
+++ programmers/Lv1/콜라 문제.js
@@ -0,0 +1,9 @@
+function solution(a, b, n) {
+  let answer = 0;
+  while (n >= a) {
+    answer = answer + Math.floor(n / a) * b;
+    n = Math.floor(n % a) + Math.floor(n / a) * b;
+  }
+
+  return answer;
+}
