42566d1e7bd16b9304856a64622d615c3182d067
jisooooooooooo <dlawltn0708@naver.com>
2024-05-12 14:26:44 +0900

    옹알이(2)

---
+++ programmers/Lv1/옹알이(2).js
@@ -0,0 +1,18 @@
+function solution(babbling) {
+  var answer = 0;
+  const word = ["aya", "ye", "woo", "ma"];
+  for (let i = 0; i < babbling.length; i++) {
+    let b = babbling[i];
+    for (let j = 0; j < word.length; j++) {
+      if (b.includes(word[j].repeat(2))) {
+        break;
+      }
+      b = b.split(word[j]).join(" ");
+    }
+    if (b.split(" ").join("").length === 0) {
+      answer += 1;
+    }
+  }
+
+  return answer;
+}
