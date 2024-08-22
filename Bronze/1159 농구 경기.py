0f04e30b07bd00b30c1b5cc1f30c85cb084e9e97
jisooooooooooo <dlawltn0708@naver.com>
2024-08-22 22:25:25 +0900

    1159 농구 경기

---
+++ Bronze/1159 농구 경기.py
@@ -0,0 +1,12 @@
+n = int(input())
+lst = [0] * 26
+
+for _ in range(n) :
+    name = input()
+    lst[ord(name[0])-97] += 1
+if max(lst) >= 5 :
+    for i in range(len(lst)) :
+        if lst[i] >= 5 :
+            print(chr(i+97), end='')
+else :
+    print("PREDAJA")
